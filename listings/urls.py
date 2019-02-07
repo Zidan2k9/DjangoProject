c7a44dea0c816fd4252688a907421847edde307b
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-02-07 22:44:38 +0000

    Individual Lisiting attributes are displayed, Listingss More Info link is routed to it's own individual listing web page

--- listings/urls.py
+++ listings/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns =[
+urlpatterns = [
     path('', views.index, name='listings'),
-    path('<int:listing_id>',views.listing,name='listing'),
-    path('search>',views.search,name='seach'),
+    path('<int:listing_id>', views.listing, name='listing'),
+    path('search',views.search, name='search'),
 ]
