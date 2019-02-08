2fab97c345082ed6cc952747eeddd4dad1cdeea8
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-02-08 00:19:06 +0000

    The realtor of the monthss photo and description is displayed on the homepage

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
@@ -16,4 +17,15 @@ def index(request):
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    #Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors':mvp_realtors
+    }
+
+    return render(request,'pages/about.html',context)
