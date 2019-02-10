681164983077b29429c4b2aa855871a5ebb78e5e
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-02-10 00:31:52 +0000

    End users can login with their username and password

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout',views.logout, name='logout'),
-    path('logout',views.dashboard, name='dashboard'),
+    path('dashboard',views.dashboard, name='dashboard'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -43,7 +43,18 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         #Login User
-        return
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
@@ -51,4 +62,4 @@ def logout(request):
     return redirect('index')
 
 def dashboard(request):
-    return render(request, 'accounts/dashbpard.html')
+    return render(request, 'accounts/dashboard.html')
