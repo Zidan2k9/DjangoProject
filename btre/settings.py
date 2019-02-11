1ff96cc04327aa5697aa8ce3f5cd886fe5760814
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-02-11 23:54:55 +0000

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='dummydata71@gmail.com'
 EMAIL_HOST_PASSWORD='johncoding'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
