20a47a49bc282886395f75ba81c6fe528af155d3
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-02-12 02:55:23 +0000

    Removed email functioanlity

--- btre/settings.py
+++ btre/settings.py
@@ -142,12 +142,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER='dummydata71@gmail.com'
-EMAIL_HOST_PASSWORD='johncoding'
-EMAIL_USE_TLS=True
+# #Email config
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER='dummydata71@gmail.com'
+# EMAIL_HOST_PASSWORD='johncoding'
+# EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
